8d168eeef61d6a87f0b440678efb7e9c77705230
demomasters <dwrdchoi@gmail.com>
2021-11-30 01:22:23 +0800

    M3: Create Simple Selection Statements

--- WiredBrainCoffeeSurveys.Reports/Program.cs
+++ WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WiredBrainCoffeeSurveys.Reports
 {
@@ -6,23 +7,16 @@ namespace WiredBrainCoffeeSurveys.Reports
     {
         static void Main(string[] args)
         {
+            var tasks = new List<string>();
+
             // Calculated Values
             double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
-            double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
             double overallScores = (Q1Results.ServiceScore + Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
 
-            Console.WriteLine($"Response Percentage: {responseRate}");
-            Console.WriteLine($"Unanswered Surveys: {unansweredCount}");
-            Console.WriteLine($"Overall Score: {overallScores}");
-
-            // Logical Comparisons
-            bool higherCoffeeScore = Q1Results.CoffeeScore > Q1Results.FoodScore;
-            bool customerRecommend = Q1Results.WouldRecommend >= 7;
-            bool noGranolaYesCappucino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
-
-            Console.WriteLine($"Coffee Score Higher than Food: {higherCoffeeScore}");
-            Console.WriteLine($"Customer Would Recommend Us: {customerRecommend}");
-            Console.WriteLine($"Hate Granola, Love Cappucino: {noGranolaYesCappucino}");
+            if (Q1Results.CoffeeScore < Q1Results.FoodScore)
+            {
+                tasks.Add("Investigate coffee receipts and ingredients.");
+            }
         }
     }
 }
